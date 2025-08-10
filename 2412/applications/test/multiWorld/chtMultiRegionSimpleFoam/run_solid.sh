ebcd01259c758199393630ef4c208cbd2cf67fd4
johnlast010190 <johnlast010190@gmail.com>
2025-08-10 15:39:04 +1000

    Delete 2412 directory

--- 2412/applications/test/multiWorld/chtMultiRegionSimpleFoam/run_solid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd "${0%/*}" || exit                                # Run from this directory
-#------------------------------------------------------------------------------
-
-chtMultiRegionSimpleFoam -case solid -world solid 2>&1 | tee run_solid.log
-read dummy
-
-#------------------------------------------------------------------------------
--- 2412/etc/caseDicts/annotated/README.md
+++
@@ -1,5 +0,0 @@
-Annotated Dictionaries
-----------------------
-
-This directory contains a collection of example dictionaries which can
-be used as a reference for some of the available options.
